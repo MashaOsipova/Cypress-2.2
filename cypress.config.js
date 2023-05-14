75e21a511976d8050e67b99f4dc715a01a4e2952
MashaOsipova <69155009+MashaOsipova@users.noreply.github.com>
2023-05-14 16:17:46 +0300

    init

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  projectId: "7scsa2"
+}
+module.exports = {
+  projectId: '7scsa2',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+};
